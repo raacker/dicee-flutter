63409fce376bb4ff35aa2222a3e3390af0030467
raacker <haven.cpp@gmail.com>
2020-04-28 20:59:04 +0900

    final version

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+import 'dart:core';
 
 void main() {
   return runApp(
@@ -18,6 +20,62 @@ void main() {
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: DicesWidget(),
+    );
+  }
+}
+
+class DicesWidget extends StatefulWidget {
+  @override
+  State<DicesWidget> createState() => _DicesWidgetState();
+}
+
+class _DicesWidgetState extends State<DicesWidget> {
+  Random r = Random(DateTime.now().millisecondsSinceEpoch);
+
+  int _leftRoll = 1;
+  int _rightRoll = 1;
+
+  int getNextRoll()
+  {
+    return r.nextInt(6) + 1;
+  }
+
+  _DicesWidgetState()
+  {
+    _leftRoll = getNextRoll();
+    _rightRoll = getNextRoll();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      onPressed: () {
+        setState(() {
+          _leftRoll = getNextRoll();
+          _rightRoll = getNextRoll();
+        });
+      },
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Expanded(
+            flex: 1,
+            child: Container(
+              padding: EdgeInsets.all(16),
+              child: Image.asset('images/dice$_leftRoll.png'),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child:  Container(
+              padding: EdgeInsets.all(16),
+              child: Image.asset('images/dice$_rightRoll.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
